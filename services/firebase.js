66ed5e3aa6b5408658ff35ab28be3ce5b842f8f4
sondreoanas <sondreoanas@gmail.com>
2022-10-01 00:27:05 +0200

    feat: firebase service

--- services/firebase.js
+++ services/firebase.js
@@ -20,3 +20,27 @@ export async function SetProjects(data) {
   return await setDoc(doc(db, 'ProjectPage', 'Projects'), data);
 }
 
+export async function GetContent() {
+  const content = await getDoc(doc(db,'ProjectPage', 'ExitSlider'));
+  return content.data().Content;
+}
+
+export async function GetPhotos() {
+  return await getDoc(doc(db,'ProjectPage', 'PhotoPage')); 
+}
+
+export async function SetPhotos(data) {
+  return await setDoc(doc(db, 'ProjectPage', 'PhotoPage'), data);
+}
+
+export async function GetFileUrl(filename) {
+  return await getDownloadURL(ref(storage, filename));
+}
+
+export async function GetPhotoUrls(filenames) {
+  const photoUrls = [];
+  for (const photo of filenames) {
+    photoUrls.push(await GetFileUrl(photo));
+  }
+  return photoUrls;
+}
