eb11bcd80c945a683222d8d867770fb1f150b353
Sondre J. Oanæs <sondreoanas@gmail.com>
2022-10-01 00:29:55 +0200

    fix: well...

--- firebaseconfig.js
+++
@@ -1,9 +0,0 @@
-export default {
-    apiKey: "AIzaSyA8xzwEE_NfiO_0nEsGT0IJq__hRVWbRog",
-    authDomain: "faux-majeure.firebaseapp.com",
-    projectId: "faux-majeure",
-    storageBucket: "faux-majeure.appspot.com",
-    messagingSenderId: "295098820046",
-    appId: "1:295098820046:web:904c9706c8e834f9dc379f",
-    measurementId: "G-DF6Y6PV5J7"
-};
